7dc63a193a0cda519009d53bad8f988914ae046d
Aaron Chong <aaronchongth@gmail.com>
2020-12-06 16:45:47 +0800

    Refactored 02 into main
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- main.py
+++ main.py
@@ -40,10 +40,50 @@ def solve_01():
 
 # 02 password philosophy
 
+def split_low_high_char_pw(line):
+    low_split = line.split('-')
+    low = int(low_split[0])
+
+    high_split = low_split[1].split(' ')
+    high = int(high_split[0])
+
+    char_with_colon = high_split[1]
+    pw = high_split[2]
+
+    char = char_with_colon[:-1]
+
+    return low, high, char, pw
+
+
 def solve_02():
-    raise NotImplementedError
+    file_name = '02_password_philosophy/input.txt'
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    
+    low_high_valid = 0
+    occurrence_valid = 0
+    for l in lines:
+        low, high, char, pw = split_low_high_char_pw(l[:-1])
+        # print(pw)
+        occurrence = 0
+        for c in pw:
+            if c == char:
+                occurrence += 1
+        if occurrence >= low and occurrence <= high:
+            low_high_valid += 1
+
+        if pw[low - 1] == char and pw[high - 1] == char:
+            continue
+        elif pw[low - 1] == char or pw[high - 1] == char:
+            occurrence_valid += 1
+        else:
+            continue
+
+    # raise NotImplementedError
+    print(low_high_valid)
+    print(occurrence_valid)
 
 
 if __name__ == '__main__':
-    solve_01()
+    # solve_01()
     solve_02()
